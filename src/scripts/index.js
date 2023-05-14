385b23d27e6aa6757976e25af00ba953a5c65ad3
Tarasdeveloper <tomahavki1@gmail.com>
2023-05-14 17:09:19 +0300

    Updated

---
+++ src/scripts/index.js
@@ -0,0 +1,27 @@
+import '../css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import { makeMarkup } from './makeMarkup';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { refs } from './makeMarkup';
+
+const DEBOUNCE_DELAY = 300;
+
+// const { inputRef, countryListRef, countryInfoRef } = refs;
+
+refs.inputRef.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(event) {
+  let searchCountry = event.target.value.trim();
+  refs.countryInfoRef.innerHTML = '';
+  refs.countryListRef.innerHTML = '';
+
+  if (searchCountry) {
+    fetchCountries(searchCountry)
+      .then(makeMarkup)
+      .catch(error => {
+        console.log(error);
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      });
+  }
+}
---
+++ src/scripts/makeMarkup.js
@@ -0,0 +1,54 @@
+import Notiflix from 'notiflix';
+// import { refs } from './refs';
+export const refs = {
+  inputRef: document.querySelector('#search-box'),
+  countryListRef: document.querySelector('.country-list'),
+  countryInfoRef: document.querySelector('.country-info'),
+};
+
+// const { countryListRef, countryInfoRef } = refs;
+
+export function makeMarkup(value) {
+  if (value.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  }
+
+  if (value.length >= 2 && value.length <= 10) {
+    const markup = value
+      .map(
+        elem =>
+          `
+            <li class="list-item">
+                <img src="${elem.flags.svg}" width="40px">
+                <span>${elem.name.official}</span>
+            </li>
+        `
+      )
+      .join('');
+    refs.countryListRef.insertAdjacentHTML('beforeend', markup);
+  }
+
+  if (value.length === 1) {
+    const markup = value
+      .map(
+        elem =>
+          `
+                <div>
+                    <div class="countryflag">
+                      <img src="${elem.flags.svg}" width="40px">
+                      <h1 class="country-name">${elem.name.official}</h1>
+                    </div>
+                    <p>Capital: ${elem.capital}</p>
+                    <p>Population: ${elem.population}</p>
+                    <p>Languages: ${Object.values(elem.languages)
+                      .map(elem => elem)
+                      .join(', ')}</p>
+                </div>
+            `
+      )
+      .join('');
+    refs.countryInfoRef.insertAdjacentHTML('beforeend', markup);
+  }
+}
